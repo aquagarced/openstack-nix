91c9f56efa3a6026be307045d5d96ab2b9707e3c
Andres Garced <148395623+aquagarced@users.noreply.github.com>
2025-08-24 16:56:00 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
           (final: prev: {
             pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [ (pyfinal: pyprev: {
                 horizon = pyfinal.callPackage ./packages/horizon.nix {inherit (nixpkgs) callPackage python3Packages;};
-                django = nixpkgs.python3Packages.django;
+                django = nixpkgs.legacyPackages.x86_64-linux.python3Packages.django;
             })];
           })
         ];};
