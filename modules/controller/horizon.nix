86ebff1186683b2c3c32aa3bddac24c541eab145
Andres Garced <148395623+aquagarced@users.noreply.github.com>
2025-08-24 04:44:00 +0000

    Update horizon.nix

--- modules/controller/horizon.nix
+++ modules/controller/horizon.nix
@@ -1,4 +1,4 @@
-{  }:
+{ horizon-pkg }:
 {
   config,
   lib,
@@ -26,7 +26,7 @@ in
       '';
     };
     package = mkOption {
-      default = horizon;
+      default = horizon-pkg;
       description = ''The Horizon Package to use'';
       type = types.package;
     };
