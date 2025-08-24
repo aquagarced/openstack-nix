f09e182ddafd2c04e996d3d7ffc0590532a974ac
Andres Garced <148395623+aquagarced@users.noreply.github.com>
2025-08-24 06:00:08 +0000

    Update openstack-controller.nix

--- modules/controller/openstack-controller.nix
+++ modules/controller/openstack-controller.nix
@@ -30,7 +30,7 @@ in
     (import ./placement.nix { inherit placement; })
     (import ./nova.nix { inherit nova; })
     (import ./neutron.nix { inherit neutron; })
-    (import ./horizon.nix { horizon-pkg = horizon; })
+    (import ./horizon.nix { inherit horizon; })
   ];
 
   config = {
